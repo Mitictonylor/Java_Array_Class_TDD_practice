120315778de5938aa97c877e2e3f765748a9472d
Antonio Lorusso <mitictonylor@gmail.com>
2020-05-26 14:13:09 +0100

    add passengerCount(), test pass

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class Bus {
+
+    private String destination;
+    private int capacity;
+    private ArrayList<Person> passengers;
+
+
+    public Bus(String destination){
+        this.destination = destination;
+        this.capacity = 40;
+        this.passengers = new ArrayList<Person>();
+    }
+
+    public int passengerCount() {
+        return this.passengers.size();
+    }
+
 }
--- src/test/java/BusTest.java
+++ src/test/java/BusTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BusTest {
+
+    private Bus bus;
+    private Person passenger;
+
+    @Before
+    public void before(){
+        bus = new Bus( "Glasgow Central");
+        passenger = new Person();
+    }
+    @Test
+    public void howManyPeopleAreOnTheBus(){
+                assertEquals(0, bus.passengerCount());
+    }
+
 }
